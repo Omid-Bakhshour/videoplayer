8230336fe1661fed270e210b2d3d5df8990d4934
Omid <fishvardroid@gmail.com>
2024-06-08 20:00:38 +0400

    add volume slider

--- src/components/Controls/Volume/VolumeSlider.tsx
+++ src/components/Controls/Volume/VolumeSlider.tsx
@@ -1,9 +1,11 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { VideoRefType } from '@/models/player';
+import { throttle } from 'lodash';
 
 const VolumeSlider: React.FC<VideoRefType> = ({ videoRef }) => {
   const [volume, setVolume] = useState<number>(1);
   const sliderRef = useRef<HTMLDivElement | null>(null);
+  const isDragging = useRef(false);
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
@@ -19,26 +21,73 @@ const VolumeSlider: React.FC<VideoRefType> = ({ videoRef }) => {
     }
   }, [videoRef, volume]);
 
-  const handleSliderClick = (event: React.MouseEvent<HTMLDivElement>) => {
+  const updateVolume = (clientX: number) => {
     if (sliderRef.current && videoRef.current) {
       const sliderWidth = sliderRef.current.offsetWidth;
-      const clickPosition = event.nativeEvent.offsetX;
-      const newVolume = clickPosition / sliderWidth;
+      const sliderLeft = sliderRef.current.getBoundingClientRect().left;
+      const clickPosition = clientX - sliderLeft;
+      const newVolume = Math.min(Math.max(clickPosition / sliderWidth, 0), 1);
 
       videoRef.current.volume = newVolume;
       setVolume(newVolume);
-      localStorage?.setItem('volume', JSON.stringify(newVolume));
+      localStorage.setItem('volume', JSON.stringify(newVolume));
     }
   };
+
+  const throttledUpdateVolume = throttle(updateVolume, 50);
+
+  const handleMouseDown = (event: React.MouseEvent<HTMLDivElement>) => {
+    isDragging.current = true;
+    updateVolume(event.clientX);
+  };
+
+  const handleMouseMove = (event: MouseEvent) => {
+    if (isDragging.current) {
+      throttledUpdateVolume(event.clientX);
+    }
+  };
+
+  const handleMouseUp = () => {
+    isDragging.current = false;
+  };
+
+  const handleTouchStart = (event: React.TouchEvent<HTMLDivElement>) => {
+    isDragging.current = true;
+    updateVolume(event.touches[0].clientX);
+  };
+
+  const handleTouchMove = (event: TouchEvent) => {
+    if (isDragging.current) {
+      throttledUpdateVolume(event.touches[0].clientX);
+    }
+  };
+
+  const handleTouchEnd = () => {
+    isDragging.current = false;
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener('mouseup', handleMouseUp);
+    document.addEventListener('touchmove', handleTouchMove);
+    document.addEventListener('touchend', handleTouchEnd);
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+      document.removeEventListener('mouseup', handleMouseUp);
+      document.removeEventListener('touchmove', handleTouchMove);
+      document.removeEventListener('touchend', handleTouchEnd);
+    };
+  }, []);
 
   return (
     <div className='relative'>
       <div
         className='w-[100px] h-[6px] relative cursor-pointer'
-        onClick={handleSliderClick}
+        onMouseDown={handleMouseDown}
+        onTouchStart={handleTouchStart}
         ref={sliderRef}
       >
-        {/* Initial background */}
         <div className='w-full h-full block rounded-full bg-slate-600' />
         {/* Progress */}
         <div
